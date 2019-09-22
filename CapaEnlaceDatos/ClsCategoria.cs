f7750eb3278128360839e78ed6fbe3f6d5e9da97
Victor Alvarado <alvaradoguzman79@hotmail.com>
2019-09-21 18:02:59 -0600

    Se cambio clase parametros por clase categoria

---
+++ CapaEnlaceDatos/ClsCategoria.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaEnlaceDatos
+{
+    public class ClsCategoria
+    {
+        private ClsConexion conexion = new ClsConexion();
+
+        SqlDataReader leer;
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
+
+        public DataTable Listar()
+        {
+
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "ListarCategoria";
+            comando.CommandType = CommandType.StoredProcedure;
+            leer = comando.ExecuteReader();
+            tabla.Load(leer);
+            conexion.CerrarConexion();
+            return tabla;
+
+        }
+
+        public void Registrar(string nombreCategoria)
+        {
+            //PROCEDIMNIENTO
+
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "RegistrarCategoria";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@nombreCategoria", nombreCategoria);
+            comando.ExecuteNonQuery();
+
+            comando.Parameters.Clear();
+
+        }
+
+        public void Modificar(int idCategoria, string nombreCategoria)
+        {
+
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "ModificarCategoria";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@nombreCategoria", nombreCategoria);
+            comando.Parameters.AddWithValue("@idCategoria", idCategoria);
+
+            comando.ExecuteNonQuery();
+
+            comando.Parameters.Clear();
+        }
+
+        public void Eliminar(int idCategoria)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EliminarCategoria";
+            comando.CommandType = CommandType.StoredProcedure;
+
+            comando.Parameters.AddWithValue("@idCategoria", idCategoria);
+
+            comando.ExecuteNonQuery();
+
+            comando.Parameters.Clear();
+        }
+    }
+}
