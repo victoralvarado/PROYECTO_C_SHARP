bc6bd453eccc46dacf65cfebe9e856fd3a71a4f0
Victor Alvarado <alvaradoguzman79@hotmail.com>
2019-11-04 17:12:54 -0600

    Correcciones

--- CapaPresentacion/frmCategoria.cs
+++ CapaPresentacion/frmCategoria.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-
-using System.Data;
-using System.Drawing;
-
 using System.Windows.Forms;
 using CapaLogicaNegocio;
 /*
--- CapaPresentacion/frmPrincipal.cs
+++ CapaPresentacion/frmPrincipal.cs
@@ -1,5 +1,4 @@
-﻿using CapaLogicaNegocio;
-using System;
+﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
 /*
